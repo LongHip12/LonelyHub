838d9daab6e8a1c236c919bfa08fb5cc9a58dd96
LongHip12 <longhip2012@gmail.com>
2025-12-04 12:55:29 +0700

    Update LonelyHub-KaitunBF.lua

--- LonelyHub-KaitunBF.lua
+++ LonelyHub-KaitunBF.lua
@@ -1,2 +1,3 @@
-game.Players.LocalPlayer:Kick("Script đã gần hoàn thiện và đang trong giai đoạn thử nghiệm :D\nJoin Discord: dsc.gg/lonelyhub")
+game.Players.LocalPlayer:Kick("Script Down\nJoin Discord: dsc.gg/lonelyhub")
+
 
